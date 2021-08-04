b78b18b6c506acaa54daeef8c522efa9fa82814c
Peter Cock <p.j.a.cock@googlemail.com>
2021-08-04 14:10:30 +0100

    HTML in instructions; volunteering with timestamp

--- volunteer_community_centre/__init__.py
+++ volunteer_community_centre/__init__.py
@@ -14,6 +14,8 @@ class Group(BaseGroup):
 class Player(BasePlayer):
     name = models.StringField(label='What is your name?')
     age = models.IntegerField(label='What is your age?', min=18)
+    volunteer = models.BooleanField(choices=[[True, 'Yes'], [False, 'No']], initial=False, label='Do you volunteer?', widget=widgets.RadioSelectHorizontal)
+    submission_timestamp = models.FloatField()
 class Survey(Page):
     form_model = 'player'
     form_fields = ['name', 'age']
@@ -23,7 +25,11 @@ class Instructions(Page):
     form_model = 'player'
 class Volunteering(Page):
     form_model = 'player'
-    form_fields = ['age', 'name']
+    form_fields = ['volunteer']
     timeout_seconds = 15
     timer_text = 'You have 15 seconds to decide.'
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        import time  # hack, want this at top level really
+        player.submission_timestamp = time.time()
 page_sequence = [Survey, Results, Instructions, Volunteering]
