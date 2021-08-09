661a4bff52f8332fce48e68dd4f73d7768710ebe
Peter Cock <p.j.a.cock@googlemail.com>
2021-08-09 14:00:46 +0100

    Ten lottery A/B choices on single page
    Uses the 10 images added previously.
    
    Includes two new pages, intructions and decision
    
    Doing the radio buttons via template HTML - this lets
    me put the button in the same table cell as the text,
    without repetive field definitions via the oTree Studio
    UI. However there are downsides including being more
    fragile to oTree changes.
    
    Using a single column for both A and B options to
    simplify placing any field error message included via
    {{ formfield_errors field.name }} which will show
    messages like "This field is required" (in red) if
    there is a problem with the user input.
    
    (Squashed commit from a day of tinkering with this)

--- risk_attitude/__init__.py
+++ risk_attitude/__init__.py
@@ -12,7 +12,21 @@ class Subsession(BaseSubsession):
 class Group(BaseGroup):
     pass
 class Player(BasePlayer):
-    pass
+    lottery1 = models.BooleanField(label='1')
+    lottery2 = models.BooleanField(label='2')
+    lottery3 = models.BooleanField(label='3')
+    lottery4 = models.BooleanField(label='4')
+    lottery5 = models.BooleanField(label='5')
+    lottery6 = models.BooleanField(label='6')
+    lottery7 = models.BooleanField(label='7')
+    lottery8 = models.BooleanField(label='8')
+    lottery9 = models.BooleanField(label='9')
+    lottery10 = models.BooleanField(label='10')
 class Introduction(Page):
     form_model = 'player'
-page_sequence = [Introduction]
+class LotteryInstructions(Page):
+    form_model = 'player'
+class LotteryDecision(Page):
+    form_model = 'player'
+    form_fields = ['lottery1', 'lottery2', 'lottery3', 'lottery4', 'lottery5', 'lottery6', 'lottery7', 'lottery8', 'lottery9', 'lottery10']
+page_sequence = [Introduction, LotteryInstructions, LotteryDecision]
