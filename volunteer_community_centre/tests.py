2e2002da9a4f83e1b88e053010999255f6ff388f
Peter Cock <p.j.a.cock@googlemail.com>
2021-08-11 10:05:50 +0100

    Test no volunteers means no tokens

--- volunteer_community_centre/tests.py
+++ volunteer_community_centre/tests.py
@@ -1,4 +1,4 @@
-from otree.api import Bot
+from otree.api import Bot, expect
 
 from . import Instructions, Volunteering, Results
 
@@ -9,4 +9,6 @@ class PlayerBot(Bot):
             yield Instructions
         # yield WaitToStart <-- automatic
         yield Volunteering, {"volunteer": False}
+        expect("<p>Thanks for making your choice.</p>", "in", self.html)
+        expect("No one in your group volunteered. You earned 0 tokens.", 'in', self.html)
         yield Results
