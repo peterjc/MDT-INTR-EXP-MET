b4e4c3bc13eb7f1e3a7ecfe789daaac462b740a0
Peter Cock <p.j.a.cock@googlemail.com>
2021-08-11 11:15:52 +0100

    Reword: Another person in your group volunteered first

--- volunteer_community_centre/__init__.py
+++ volunteer_community_centre/__init__.py
@@ -87,7 +87,7 @@ class Results(Page):
                 msg += f"<p>You volunteered first. You earn {volunteer_payoff} tokens and the other members of your group earn {group_payoff}.</p>"
             else:
                 player.payoff = group_payoff
-                msg += f"<p>At least one person in your group volunteered first. You earn {group_payoff} tokens.</p>"
+                msg += f"<p>Another person in your group volunteered first. You earn {group_payoff} tokens.</p>"
         elif player.round_number in [3, 4]:
             # Alternative rules - compensated volunteer
             no_volunteers_payoff = 0
@@ -98,10 +98,10 @@ class Results(Page):
                 msg += f"<p>No one in your group volunteered. You earned {no_volunteers_payoff} tokens.</p>"
             elif player.submission_timestamp == min(p.submission_timestamp for p in players if p.volunteer):
                 player.payoff = volunteer_payoff
-                msg += f"<p>You volunteered. You earn {volunteer_payoff} tokens and the other members of your group earn {group_payoff} tokens.</p>"
+                msg += f"<p>You volunteered first. You earn {volunteer_payoff} tokens and the other members of your group earn {group_payoff} tokens.</p>"
             else:
                 player.payoff = group_payoff
-                msg += f"<p>At least one person in your group volunteered. You earn {group_payoff} tokens, and the person who volunteered first earns {volunteer_payoff} tokens.</p>"
+                msg += f"<p>Another person in your group volunteered first. You earn {group_payoff} tokens, and the person who volunteered first earns {volunteer_payoff} tokens.</p>"
         else:
             raise RuntimeError(f"ERROR - Unexpected round number {player.round_number}")
         if player.round_number == 1:
