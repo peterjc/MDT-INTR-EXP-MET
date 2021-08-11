383f57707fdda40bf3f74490e6f71e61f3864933
Peter Cock <p.j.a.cock@googlemail.com>
2021-08-11 11:16:55 +0100

    Test everyone volunteering; test payoffs
    No obvious way to control which bot is first, looks like
    will always be in player order if multiple bot players
    volunteer in the same round.

--- volunteer_community_centre/tests.py
+++ volunteer_community_centre/tests.py
@@ -4,13 +4,45 @@ from . import Instructions, Volunteering, Results
 
 
 class PlayerBot(Bot):
+
+    cases = ["never", "all"]
+
     def play_round(self):
         if self.round_number == 1:
             yield Instructions
         # yield WaitToStart <-- automatic
-        yield Volunteering, {"volunteer": False}
-        expect("<p>Thanks for making your choice.</p>", "in", self.html)
-        expect(
-            "No one in your group volunteered. You earned 0 tokens.", "in", self.html
-        )
+        if self.case == "never":
+            # Do with timeout?
+            yield Volunteering, {"volunteer": False}
+            expect("<p>Thanks for making your choice.</p>", "in", self.html)
+            expect(
+                "No one in your group volunteered. You earned 0 tokens.",
+                "in",
+                self.html,
+            )
+            expect(self.player.payoff, 0)
+        else:
+            yield Volunteering, {"volunteer": True}
+            expect("<p>Thanks for making your choice.</p>", "in", self.html)
+            if self.player.id_in_group == 1:
+                # Appears bot for player one is always first
+                expect("You volunteered first.", "in", self.html)
+                if self.round_number in [1, 2, 5]:
+                    expect(self.player.payoff, 0)  # self-less
+                else:
+                    expect(self.player.payoff, 50)  # compensated
+                if self.round_number == 5:
+                    # Game over, 0 + 0 + 50 + 50 + 0
+                    expect(self.participant.payoff, 100)
+            else:
+                expect(
+                    "Another person in your group volunteered first.", "in", self.html
+                )
+                if self.round_number in [1, 2, 5]:
+                    expect(self.player.payoff, 50)  # free-loader
+                else:
+                    expect(self.player.payoff, 40)  # compensation deduction
+                if self.round_number == 5:
+                    # Game over, 50 + 50 + 40 + 40 + 50
+                    expect(self.participant.payoff, 230)
         yield Results
