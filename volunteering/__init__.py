cefe115d3a1ff4299266528978b773ecf3d0c149
Peter Cock <p.j.a.cock@googlemail.com>
2022-01-14 13:50:00 +0000

    Polishing text of the volunteering game

--- volunteering/__init__.py
+++ volunteering/__init__.py
@@ -25,13 +25,13 @@ class Player(BasePlayer):
 def understanding1_error_message(player: Player, value):
     group = player.group
     if value != 0:
-        return f"Your answer is wrong, if no one in the group volunteers all of you earn {cu(0)}"
+        return f"Actually, if no one in the group volunteers all of you earn {cu(0)}."
 def understanding2_error_message(player: Player, value):
     if value != 50:
-        return f"Your answer is wrong, if someone volunteers before you, they will earn {cu(0)} but you will still earn {cu(50)}"
+        return f"Actually, if someone volunteers before you, they  earn {cu(0)}, but you still earn {cu(50)}."
 def understanding3_error_message(player: Player, value):
     if value != 0:
-        return f"Your answer is wrong, if you are the first person to volunteer, you earn {cu(0)}"
+        return f"Actually, if you are the first person to volunteer, you earn {cu(0)}."
 class WaitAndGroup(WaitPage):
     wait_for_all_groups = True
     title_text = 'Waiting for other players to begin'
@@ -55,7 +55,7 @@ class Instructions(Page):
             # framing=1 gives True, Community Centre
             framing_msg = f"""
         <p>
-        Imagine that the {C.PLAYERS_PER_GROUP} people in your group reside in the same small rural community. In your community there is a community centre where people can meet during the day to have a coffee together and do a series of activities. All the residents have access to the centre but in order to enjoy this space, someone must open it and prepare the rooms as well as make order and clean afterwards. If no one volunteers to do this, the centre remains closed. The activities implemented in the centre benefit the residents attending.  This benefit has a value of {cu(50)}, while the volunteer incurs personal costs in terms of time and energy that cancel out the benefits from the activities implemented in the centre.
+        Imagine that the {C.PLAYERS_PER_GROUP} people in your group reside in the same small rural community. In your community there is a community centre where people can meet during the day to have a coffee or tea together and do a series of activities. All the residents have access to the centre but in order to enjoy this space, someone must open it and prepare the rooms as well as tidy up and clean afterwards. If no one volunteers to do this, the centre remains closed. The activities implemented in the centre benefit all residents attending.  This benefit has a value of {cu(50)}. The volunteer incurs personal costs in terms of time and energy are also worth {cu(50)}, which means that they are left with {cu(0)}.
         </p>
         """
         else:
@@ -74,19 +74,31 @@ class Instructions(Page):
         This game consists of {C.NUM_ROUNDS} rounds. In each round, you will receive instructions and will be asked to make a decision. The decisions in each round are completely independent from each other. Your earnings from each round will depend on your decision, as well as the decisions that other participants make in that round.
         </p>
         <p>
-        The computer will pair you with other {C.PLAYERS_PER_GROUP - 1} participants, making a group of {C.PLAYERS_PER_GROUP}. The group will remain the same during the {C.NUM_ROUNDS} rounds. In each round, each of the members of your group has the opportunity to earn {cu(50)}. In order for the members of your group to earn the {cu(50)}, at least one of you needs to ‘volunteer’. The volunteer will have to pay a cost of {cu(50)}, meaning that they will not earn any {units} in that round. You will have {C.VOLUNTEER_TIMEOUT} seconds to decide whether to volunteer. If at least one participant in your group volunteers, everyone apart from them will earn {cu(50)}. Only the participant who volunteers first will have to pay the {cu(50)}, and thus will not earn any {units}. Those who will possibly volunteer after them will  not be considered as volunteers and will thus receive {cu(50)}. If no one volunteers, no one will earn any {units}. At the end of the {C.VOLUNTEER_TIMEOUT} seconds, you will be automatically redirected to the page with the results of that round. You will be told whether anyone has volunteered, and your total earnings in that round. You will not know the identity of the person who has volunteered.
+        The computer will group you with other {C.PLAYERS_PER_GROUP - 1} participants, making a group of {C.PLAYERS_PER_GROUP}. The group will remain the same during the {C.NUM_ROUNDS} rounds.
+        </p>
+        <p>
+        In each round, each of the members of your group has the opportunity to earn {cu(50)}. In order for the members of your group to earn the {cu(50)}, at least one of you needs to ‘volunteer’.
+        </p>
+        <p>
+        The volunteer will have to pay a cost of {cu(50)}, meaning that they will not earn any {units} in that round. You will have {C.VOLUNTEER_TIMEOUT} seconds to decide whether to volunteer.
+        </p>
+        <p>
+        If at least one participant in your group volunteers, everyone apart from them will earn {cu(50)}. Only the participant who volunteers first will have to pay the {cu(50)}, and thus will not earn any {units}. Those who will possibly volunteer after them will  not be considered as volunteers and will thus receive {cu(50)}.
+        </p>
+        <p>
+        If no one volunteers, no one will earn any {units}. At the end of the {C.VOLUNTEER_TIMEOUT} seconds, you will be automatically redirected to the page with the results of that round. You will be told whether anyone has volunteered, and your total earnings in that round. You will not know the identity of the person who has volunteered.
         </p>
         {framing_msg}
         <p>
         To summarise, there are 3 options:
         <ol>
-        <li>You volunteer first in your group: you will earn {cu(0)}, and the {C.PLAYERS_PER_GROUP-1} other participants will earn {cu(50)} each</li>
-        <li>You volunteer but someone else volunteers first in your group: you and {C.PLAYERS_PER_GROUP-2} other participants will earn {cu(50)} while 1 other participant who volunteered first will earn {cu(0)}.</li>
-        <li>None of the {C.PLAYERS_PER_GROUP} participants in your group volunteers: you and the other 5 participants will each receive {cu(0)}.</li>
+        <li>You volunteer first in your group: you earn {cu(0)}, and the {C.PLAYERS_PER_GROUP-1} other participants earn {cu(50)} each.</li>
+        <li>You volunteer but someone else volunteers first in your group: you and {C.PLAYERS_PER_GROUP-2} other participants earn {cu(50)} while 1 other participant who volunteered first earns {cu(0)}.</li>
+        <li>None of the {C.PLAYERS_PER_GROUP} participants in your group volunteers: you and the other 5 participants each receive {cu(0)}.</li>
         </ol>
         </p>
         <p>
-        Do you have any questions? If so, please raise your hand virtually.
+        Do you have any questions? If so, please ask us by using the Q&A function, and the experimenter will answer your question for everyone.
         </p>
         <p>
         Now please click ‘Next’, and you will be redirected to a page with questions to check your understanding of the instructions.
@@ -126,10 +138,10 @@ class Volunteering(Page):
         group = player.group
         if player.round_number in [1, 2, 5]:
             # Self-less volunteer:
-            instructions = f"<p>To volunteer you need to click the button before the end of the {C.VOLUNTEER_TIMEOUT} seconds. If no one in your group volunteers, all of you earn {cu(0)}. If one person volunteers, this person earns {cu(0)} and the other members of the group earn {cu(50)}. Only the person who volunteers first earns {cu(0)}: those who will possibly volunteer after them will receive {cu(50)}. At the end of the {C.VOLUNTEER_TIMEOUT} seconds, you will be automatically redirected to the page with the results of this round.</p>"
+            instructions = f"<p>To volunteer you need to click the button before the end of the {C.VOLUNTEER_TIMEOUT} seconds. If no one in your group volunteers, all of you earn {cu(0)}. If at least one person volunteers, the first person who volunteers earns {cu(0)} and the other {C.PLAYERS_PER_GROUP-1} members of the group earn {cu(50)}. At the end of the {C.VOLUNTEER_TIMEOUT} seconds, you will be automatically redirected to the page with the results of this round.</p>"
         else:
             # Compensated volunteer:
-            instructions = f"<p>To volunteer you need to click the button before the end of the {C.VOLUNTEER_TIMEOUT} seconds. If no one in your group volunteers, all of you earn {cu(0)}. If one person volunteers, this person earns 50 points and the other members of the group earn {cu(40)}. Only the person who volunteers first earns {cu(50)}: those who will possibly volunteer after them will receive {cu(40)}. At the end of the {C.VOLUNTEER_TIMEOUT} seconds, you will be automatically redirected to the page with the results of this round.</p>"
+            instructions = f"<p>To volunteer you need to click the button before the end of the {C.VOLUNTEER_TIMEOUT} seconds. If no one in your group volunteers, all of you earn {cu(0)}. If at least one person volunteers, the first person who volunteers earns 50 points and the other {C.PLAYERS_PER_GROUP-1} members of the group earn {cu(40)}. At the end of the {C.VOLUNTEER_TIMEOUT} seconds, you will be automatically redirected to the page with the results of this round.</p>"
         return {"instructions": instructions}
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
@@ -198,7 +210,7 @@ class Results(Page):
         # Instructions change round by round...
         if player.round_number == 1:
             msg += """
-        <p>Now we will play a second round of the game, which will follow exactly the same rules of this one. The second round will start when everyone in your group has clicked on the ‘Next’ button below, which will redirect you to a new page with the countdown. Again, you can volunteer for your group by clicking on the ‘Volunteer’ button. When you are ready, please click on the ‘Next’ button below.</p>
+        <p>Now we will play a second round of the game, which will follow exactly the same rules as the first one. The second round will start when everyone in your group has clicked on the ‘Next’ button below, which will redirect you to a new page with the countdown. Again, you can volunteer for your group by clicking on the ‘Volunteer’ button. When you are ready, please click on the ‘Next’ button below.</p>
         """
         elif player.round_number == 2:
             # TODO - pull scores from code? Tricky as this is about the NEXT round
@@ -207,18 +219,18 @@ class Results(Page):
             else:
                 framing_msg = "Imagine that the group of farmers has decided to compensate the volunteering farmer for the provision of the flood protection service provided to the catchment."
             msg += f"""
-        <p>Now we will play a third round of the game, which will follow slightly different rules. Again, at least one person in your group needs to volunteer in order for you to earn some {units}, meaning that if no one volunteers, no one will earn any {units}. As previously, if there is a volunteer, all other participants benefit by {cu(50)}. However, in this round, all of the other participants will transfer 10 of their  {cu(50)} to the volunteer. Practically this means that in this third round the volunteer will earn  {cu(50)}, while the other participants will earn {cu(40)} points. Only the participant who volunteers first will earn the  {cu(50)}; if you decide to volunteer but someone else has volunteered before you, you will earn  {cu(40)} points.</p>
+        <p>Now we will play a third round of the game, which will follow slightly different rules. Again, at least one person in your group needs to volunteer in order for you to earn some {units}, meaning that if no one volunteers, no one will earn any {units}. As previously, if there is a volunteer, all other participants benefit by {cu(50)}. However, in this round, all other participants will transfer 10 of their {cu(50)} to the volunteer. Practically this means that in this third round the volunteer will earn {cu(50)}, while the other participants will earn {cu(40)} each. Only the participant who volunteers first will earn {cu(50)}; if you decide to volunteer but someone else has volunteered before you, you will earn {cu(40)}.</p>
         <p>{framing_msg}</p>
         <p>
         To summarise, there are 3 options:
         <ol>
-        <li>You volunteer first in your group: you will earn {cu(50)}, and the {C.PLAYERS_PER_GROUP-1} other participants will earn {cu(40)} each</li>
-        <li>You volunteer but someone else volunteers first in your group: you and {C.PLAYERS_PER_GROUP-2} other participants will earn {cu(40)} while 1 other participant who volunteered first will earn {cu(50)}.</li>
-        <li>None of the {C.PLAYERS_PER_GROUP} participants in your group volunteers: you and the other 5 participants will each receive {cu(0)}.</li>
+        <li>You volunteer first in your group: you earn {cu(50)}, and the {C.PLAYERS_PER_GROUP-1} other participants earn {cu(40)} each.</li>
+        <li>You volunteer but someone else volunteers first in your group: you and {C.PLAYERS_PER_GROUP-2} other participants earn {cu(40)} while 1 other participant who volunteered first earns {cu(50)}.</li>
+        <li>None of the {C.PLAYERS_PER_GROUP} participants in your group volunteers: you and the other 5 participants each receive {cu(0)}.</li>
         </ol>
         </p>
-        <p>Do you have any questions? If so, please raise your hand virtually. A member of the experimental team will answer your question for everyone.</p>
-        <p>The third round will start when everyone in your group has clicked on the ‘Next’ button below, which will redirect you to a new page with the countdown. Again, you can volunteer for your group by clicking on the ‘Volunteer’ button. When you are ready, please click on the ‘Next’ button below. The instructions will remain available at the bottom of the page</p>
+        <p>Do you have any questions? If so, please ask us by using the Q&A function, and the experimenter will answer your question for everyone.</p>
+        <p>The third round will start when everyone in your group has clicked on the ‘Next’ button below, which will redirect you to a new page with the countdown. Again, you can volunteer for your group by clicking on the ‘Volunteer’ button. When you are ready, please click on the ‘Next’ button below. The instructions will remain available at the bottom of the page.</p>
         """
         elif player.round_number == 3:
             msg += """
@@ -231,9 +243,9 @@ class Results(Page):
             else:
                 framing_msg = "Imagine that after some discussions, it was decided in the catchment to lift the compensation scheme for the farmer who volunteers; therefore, the volunteer will not be compensated any more."
             msg += f"""
-        <p>Now we will play the final round of this game, which will follow exactly the same rules of the first two rounds.<p>
+        <p>Now we will play the final round of this game, which will follow exactly the same rules as the first two rounds.<p>
         <p>{framing_msg}</p>
-        <p>The volunteer will have to pay a cost of {cu(50)}, meaning that they will not earn any {units}. If at least one participant volunteers, everyone apart from them will earn {cu(50)}. Only the participant who volunteers first will have to pay the {cu(50)}, and thus overall will not earn any {units}. Anyone who volunteers after them will earn {cu(50)}. If no one volunteers, no one will earn any {units}.</p>
+        <p>The volunteer will have to pay a cost of {cu(50)}, meaning that they will not earn any {units}. If at least one participant volunteers, everyone will earn {cu(50)}. Only the participant who volunteers first will have to pay the {cu(50)}, and thus overall will not earn any {units}. Anyone who volunteers after them will earn {cu(50)} overall. If no one volunteers, no one will earn any {units}.</p>
         <p>The final round will start when everyone in your group has clicked on the ‘Next’ button below, which will redirect you to a new page with the countdown. Again, you can volunteer for your group by clicking on the ‘Volunteer’ button. When you are ready, please click on the ‘Next’ button below. The instructions will remain available at the bottom of the page.</p>
         """
         elif player.round_number == C.NUM_ROUNDS:
